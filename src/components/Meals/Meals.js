9688abad3cd271c22f114abeaa48e476a84f487a
usama2333 <usama.wizz@gmail.com>
2023-02-20 23:10:28 +0500

    First commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,16 @@
+import React , {Fragment} from 'react'
+import AvaliableMeals from './AvaliableMeals';
+import MealsSummary from './MealsSummary';
+
+const Meals = () => {
+  return (
+    <Fragment>
+    
+      <MealsSummary/>
+      <AvaliableMeals/>
+      
+    </Fragment>
+  )
+}
+
+export default Meals;
